bb7266b0a3183580936c52e003b3ed6c68cfd627
ashtilawat23 <ashtilawat23@gmail.com>
2020-12-11 11:17:34 -0500

    Initial commit, just started

---
+++ src/theme.js
@@ -0,0 +1,26 @@
+const theme = {
+    backgroundColor: "#333",
+    backgroundColorTrsp: "#3338",
+    mainTextColor: "silver",
+    headTextColor: "yellow",
+    highlightColor: "cyan",
+
+    nodeColors: {
+        blue: "steelblue",
+        fair: "peachpuff",
+        blond: "moccasin",
+        "white, blue": "cornflowerblue",
+        "white, red": "crimson",
+        "blue-gray": "lightsteelblue",
+        gold: "goldenrod",
+        red: "firebrick",
+        light: "papayawhip",
+        yellow: "gold",
+        brown: "saddlebrown",
+        "brown, grey": "tan",
+        "auburn, white": "antiquewhite",
+        black: "gray",
+    },
+}
+
+export default theme;
